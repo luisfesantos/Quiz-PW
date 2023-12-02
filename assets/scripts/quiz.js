338bccdb0c50f97fa70815f3d310e33b6114453c
Aluno <aluno@etec.sp.gov.br>
2023-12-01 22:45:35 -0300

    Quiz_01_12_2023

--- assets/scripts/quiz.js
+++ assets/scripts/quiz.js
@@ -7,18 +7,22 @@ let qtdePulos = 0;
 let qtdeErros = 0;
 
 // Placar
+let spanNivel = document.getElementById('spanNivel');
+let spanPontos = document.getElementById('spanPontos');
 let spanPulos = document.getElementById('spanPulos');
-
+let spanErros = document.getElementById('spanErros');
 
 // Botoes
-let btnConfirmar = document.getElementById('bntConfirmar');
+let btnConfirmar = document.getElementById('btnConfirmar');
+btnConfirmar.addEventListener('click', () => confirmar());
+
 let btnPular = document.getElementById('btnPular');
 btnPular.addEventListener('click', () => pular());
 
 let btnParar = document.getElementById('btnParar');
 btnParar.addEventListener('click', () => parar());
 
-
+//Pergunta e Respostas
 let pergunta = document.getElementById('pergunta');
 let resposta01 = document.getElementById('resposta01');
 let resposta02 = document.getElementById('resposta02');
@@ -51,8 +55,36 @@ function pular(){
 
 }
 
-
 function parar(){
     alert('Que Pena, você desistiu!!');
     history.back();
+}
+
+function confirmar(){
+    let valorResposta = document.querySelector('input[name="resposta"]:checked');
+    console.log(valorResposta);
+    
+    if (valorResposta == null) {
+        alert("Selecione uma resposta antes de confirmar !");
+        return;
+    }
+
+    if (valorResposta.value == perguntas[idxPergunta].CERTA) {
+        alert('Resposta... Exata !!');
+        qtdePontos++;
+    } else {
+        alert('Resposta... Errada !!');
+        qtdeErros++;
+    }
+
+    valorResposta.checked = false;
+    sortear();
+    atualizarPlacar();
+}
+
+function atualizarPlacar(){
+    spanNivel.innerText = `Nivel: ${nivel}`;
+    spanPontos.innerText = `Pontos: ${qtdePontos}`;
+    spanPulos.innerText = `Pulos: ${qtdePulos}`;
+    spanErros.innerText = `Erros: ${qtdeErros}`;
 }
